b5f38bac535c494e1595bdf82a36ff85748f5755
aaronCruise <aaron.m.cruz@rutgers.edu>
2026-02-22 22:41:18 -0500

    Add data models for representing asset classes & portfolio

---
+++ rebalancer/models.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, field
+
+@dataclass
+class AssetClass:
+    name: str
+    target_allocation: float  # As a decimal (ex, 0.60 for 60%)
+    current_balance: float
+
+@dataclass
+class Portfolio:
+    assets: list[AssetClass] = field(default_factory=list)
+
+    @property
+    def total_value(self) -> float:
+        """Calculates the total value across all asset classes."""
+        return sum(asset.current_balance for asset in self.assets)
+
+    def validate(self) -> bool:
+        """Ensures the portfolio is valid in that the target allocations sum to 100%."""
+        total_allocation = sum(asset.target_allocation for asset in self.assets)
+        # Using round to account for floating point math quirks
+        return round(total_allocation, 4) == 1.0
